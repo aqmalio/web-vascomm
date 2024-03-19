739920f3a859475500e8fccc8ee545e342ed4fc2
aqmal.arktivak <aqmal@arktivak.io>
2024-03-19 23:56:49 +0700

    first version

---
+++ database/migrations/2024_03_19_165132_create_products_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 15, 2);
+            $table->string('gambar_produk')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+        <div class="flex flex-col bg-white">
+        <div class="flex flex-col pt-4 w-full bg-white max-md:max-w-full">
+            <div
+            class="flex gap-5 justify-between items-center self-center px-5 w-full max-w-[1284px] max-md:flex-wrap max-md:max-w-full"
+            >
+            <img
+                loading="lazy"
+                src="/images/logo.png"
+                class="shrink-0 self-stretch my-auto max-w-full aspect-[6.25] w-[168px]"
+            />
+            <div
+                class="flex gap-5 justify-between self-stretch px-4 py-1.5 my-auto text-xs tracking-normal rounded-sm bg-stone-50 text-stone-400 max-md:flex-wrap max-md:max-w-full"
+            >
+                <div class="flex-auto my-auto">Cari parfum kesukaanmu</div>
+                <img
+                loading="lazy"
+                src=""
+                class="shrink-0 w-5 aspect-square"
+                />
+            </div>
+            <div
+                class="flex gap-4 self-stretch text-sm font-bold whitespace-nowrap tracking-[3.5px]"
+            >
+                <div
+                class="justify-center px-4 py-3 text-blue-500 border border-blue-500 border-solid"
+                >
+                MASUK
+                </div>
+                <div class="justify-center px-4 py-3 text-orange-100 bg-blue-500">
+                DAFTAR
+                </div>
+            </div>
+            </div>
+            <div
+            class="mt-3.5 w-full bg-neutral-200 min-h-[1px] max-md:max-w-full"
+            ></div>
+        </div>
+        <div
+            class="flex flex-col self-center px-5 mt-8 w-full max-w-[1087px] max-md:max-w-full"
+        >
+            <img
+            loading="lazy"
+            srcset="/images/header.png"
+            class="self-center w-full aspect-[2.78] max-w-[1006px] max-md:max-w-full"
+            />
+            <div
+            class="self-start mt-10 ml-5 text-2xl font-bold tracking-tight text-stone-900 max-md:ml-2.5"
+            >
+            Terbaru
+            </div>
+            <div
+            class="flex gap-5 justify-between mt-10 text-sm font-bold max-md:flex-wrap max-md:max-w-full"
+            >
+            <img
+                loading="lazy"
+                src=""
+                class="shrink-0 my-auto w-4 border border-black border-solid aspect-[0.49] stroke-[1px] stroke-black"
+            />
+            <div
+                class="flex flex-col items-start pt-4 pr-9 pl-4 bg-white border border-solid shadow-sm border-zinc-300 max-md:pr-5"
+            >
+                <img
+                loading="lazy"
+                srcset="/images/v7_266.png"
+                class="ml-5 w-28 aspect-[0.74] max-md:ml-2.5"
+                />
+                <div class="mt-8 tracking-tight leading-[114%] text-stone-900">
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <img
+                loading="lazy"
+                src=""
+                class="shrink-0 my-auto border border-black border-solid aspect-[0.5] stroke-[1px] stroke-black w-[17px]"
+            />
+            </div>
+            <div
+            class="self-start mt-14 ml-4 text-2xl font-bold tracking-tight text-stone-900 max-md:mt-10 max-md:ml-2.5"
+            >
+            Produk Tersedia
+            </div>
+            <div
+            class="flex gap-5 self-center mt-10 w-full text-sm font-bold max-w-[1009px] max-md:flex-wrap max-md:max-w-full"
+            >
+            <div
+                class="flex flex-col flex-1 items-start pt-4 pr-9 pl-4 bg-white border border-solid shadow-sm border-zinc-300 max-md:pr-5"
+            >
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-5 w-28 aspect-[0.74] max-md:ml-2.5"
+                />
+                <div class="mt-8 tracking-tight leading-[114%] text-stone-900">
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col flex-1 px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col flex-1 px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col flex-1 px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            <div class="flex flex-col flex-1 px-4 py-8 bg-white max-md:pr-5">
+                <img
+                loading="lazy"
+                srcset="..."
+                class="ml-2.5 aspect-[0.98] w-[130px]"
+                />
+                <div
+                class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                >
+                Euodia
+                </div>
+                <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+            </div>
+            </div>
+            <div class="mt-8 ml-4 max-w-full w-[596px]">
+            <div class="flex gap-5 max-md:flex-col max-md:gap-0">
+                <div class="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+                <div
+                    class="flex flex-col grow px-4 py-8 w-full text-sm font-bold bg-white max-md:pr-5 max-md:mt-6"
+                >
+                    <img
+                    loading="lazy"
+                    srcset="..."
+                    class="ml-2.5 aspect-[0.98] w-[130px]"
+                    />
+                    <div
+                    class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                    >
+                    Euodia
+                    </div>
+                    <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+                </div>
+                </div>
+                <div class="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+                <div
+                    class="flex flex-col grow px-4 py-8 w-full text-sm font-bold bg-white max-md:pr-5 max-md:mt-6"
+                >
+                    <img
+                    loading="lazy"
+                    srcset="..."
+                    class="ml-2.5 aspect-[0.98] w-[130px]"
+                    />
+                    <div
+                    class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                    >
+                    Euodia
+                    </div>
+                    <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+                </div>
+                </div>
+                <div class="flex flex-col ml-5 w-[33%] max-md:ml-0 max-md:w-full">
+                <div
+                    class="flex flex-col grow px-4 py-8 w-full text-sm font-bold bg-white max-md:pr-5 max-md:mt-6"
+                >
+                    <img
+                    loading="lazy"
+                    srcset="..."
+                    class="ml-2.5 aspect-[0.98] w-[130px]"
+                    />
+                    <div
+                    class="mt-10 tracking-tight leading-[114%] text-stone-900 max-md:mt-10"
+                    >
+                    Euodia
+                    </div>
+                    <div class="mt-3 text-blue-500">IDR x.xxx.980</div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div
+            class="justify-center self-center px-4 py-2.5 mt-3 text-sm font-semibold text-blue-500 border border-blue-500 border-solid"
+            >
+            Lihat lebih banyak
+            </div>
+        </div>
+        <div
+            class="flex flex-col pb-20 mt-14 w-full bg-white max-md:mt-10 max-md:max-w-full"
+        >
+            <div class="w-full bg-neutral-200 min-h-[1px] max-md:max-w-full"></div>
+            <div class="mt-20 mb-2.5 ml-28 max-w-full w-[872px] max-md:mt-10">
+            <div class="flex gap-5 max-md:flex-col max-md:gap-0">
+                <div class="flex flex-col w-[39%] max-md:ml-0 max-md:w-full">
+                <div
+                    class="flex flex-col grow text-xs leading-4 text-center text-stone-900 max-md:mt-10"
+                >
+                    <img
+                    loading="lazy"
+                    src=""
+                    class="ml-6 max-w-full aspect-[6.25] w-[168px] max-md:ml-2.5"
+                    />
+                    <div class="mt-12 max-md:mt-10">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                    commodo in vestibulum, sed dapibus tristique nullam.
+                    </div>
+                    <img
+                    loading="lazy"
+                    src=""
+                    class="self-center mt-16 max-w-full aspect-[5.26] w-[103px] max-md:mt-10"
+                    />
+                </div>
+                </div>
+                <div class="flex flex-col ml-5 w-[61%] max-md:ml-0 max-md:w-full">
+                <div class="max-md:mt-10 max-md:max-w-full">
+                    <div class="flex gap-5 max-md:flex-col max-md:gap-0">
+                    <div class="flex flex-col w-[39%] max-md:ml-0 max-md:w-full">
+                        <div
+                        class="flex flex-col text-xs text-stone-900 tracking-[3px] max-md:mt-10"
+                        >
+                        <div class="text-lg tracking-tighter">Layanan</div>
+                        <div class="mt-9">BANTUAN</div>
+                        <div class="mt-6">TANYA JAWAB</div>
+                        <div class="mt-5">HUBUNGI KAMI</div>
+                        <div class="mt-5">CARA BERJUALAN</div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col ml-5 w-[42%] max-md:ml-0 max-md:w-full">
+                        <div
+                        class="flex flex-col grow text-xs text-stone-900 tracking-[3px] max-md:mt-10"
+                        >
+                        <div class="text-lg tracking-tighter">Tentang Kami</div>
+                        <div class="mt-9">ABOUT US</div>
+                        <div class="mt-6">KARIR</div>
+                        <div class="mt-5">BLOG</div>
+                        <div class="mt-5">KEBIJAKAN PRIVASI</div>
+                        <div class="mt-5">SYARAT DAN KETENTUAN</div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col ml-5 w-[19%] max-md:ml-0 max-md:w-full">
+                        <div
+                        class="flex flex-col whitespace-nowrap text-stone-900 max-md:mt-10"
+                        >
+                        <div class="text-lg tracking-tighter">Mitra</div>
+                        <div class="mt-10 text-xs tracking-[3px]">SUPPLIER</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="w-full bg-cyan-50 min-h-[24px] max-md:max-w-full"></div>
+        </div>
+    </body>
+</html>
